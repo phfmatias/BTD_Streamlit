4b16a4d28b43ca95dc3a39d222b9f81c3c5076d3
phfmatias <pedrohfmatias@gmail.com>
2025-03-13 12:08:39 -0300

    Add new methods to load SHAP models and drawn DataFrame; refine data loading process

--- srcCode/backend.py
+++ srcCode/backend.py
@@ -14,12 +14,13 @@
 
 ### IMPORTS ###
 
-from numpy import array, concatenate
-from pandas import read_csv
-from rdkit.Chem import AllChem
+from numpy import array, concatenate, random
+from pandas import read_csv, DataFrame
+from rdkit.Chem import PandasTools, AllChem, rdFingerprintGenerator
 from rdkit import Chem
 from rdkit.Chem.Fingerprints.FingerprintMols import FingerprintMol
 from rdkit.DataStructs import FingerprintSimilarity
+from random import seed
 import pickle
 from warnings import filterwarnings
 filterwarnings('ignore')
@@ -30,7 +31,11 @@ class Backend:
     def __init__(self):
         self._loadData()
         self._loadModels()
+        self._loadShap()
+        self._loadDF_Drawn()
         self.dataBaseMol = self._generateMol(self.data)
+        self.features_names = ['MF_'+str(i) for i in range(2048)] + ['etn']
+
 
     def _loadData(self):
         self.data = read_csv('Data/RafaelDB.csv')
@@ -38,6 +43,29 @@ class Backend:
         self.dataDL = dataDL[['molecule','smiles','max_abs','max_em', 'solvent', 'etn', 'doi']]
 
         self.morganBase = read_csv('Data/morgan.csv')
+
+    def _loadDF_Drawn(self):
+        
+        data = read_csv('Data/RafaelDB.csv')
+        seed(0)
+        random.seed(0)
+
+        PandasTools.AddMoleculeColumnToFrame(data, 'smiles', 'ROMol')
+                # Create the fingerprint generator
+        fp_gen = rdFingerprintGenerator.GetMorganGenerator(radius = 4, fpSize = 2048)
+
+        # Generate fingerprints for each molecule
+        morgan_fps = [list(fp_gen.GetFingerprint(m)) for m in data['ROMol']]
+
+        morgan_df = DataFrame(morgan_fps)
+
+
+        morgan_df.columns = ['MF_' + str(j) for j in morgan_df.columns]
+        morgan_df['etn'] = data['etn'].to_numpy()
+
+        self.df_drawn = morgan_df
+
+
 
     def _generateMol(self, df):
         mols = []
@@ -74,7 +102,7 @@ class Backend:
     
     def _solventToEtn(self, solvent):
 
-        translate = {'Dimethylsulfoxide (0.444)': 0.444, 'Water (1.0)': 1.0, 'Dichloromethane (0.309)': 0.309, 'Acetonitrile (0.46)': 0.46, 'Tolune (0.099)': 0.099, 'Ethanol (0.654)': 0.654, 'Chloroform (0.259)': 0.259, 'Methanol (0.762)': 0.762, 'Dimethylformamide (0.386)': 0.386, 'Cyclochexane (0.006)': 0.006, 'Hexane (0.009)': 0.009, 'Tetrahidrofurane (0.605)': 0.605, 'Methyl Cianide (0.46)': 0.46, 'Acetate (0.355)': 0.355, 'Methyl Phenil (0.099)': 0.099, 'Isopropanol (0.546)': 0.546, 'Dioxane (0.164)': 0.164, 'mXylene (0.074)': 0.074, 'Chlorobenzene (0.333)': 0.333, 'Ethyl Acetate (0.228)': 0.228, 'DiethylEter (0.117)': 0.117, 'Octanol (0.537)': 0.537, 'Nitrobenzila (0.333)': 0.333, 'Benzene (0.111)': 0.111, 'Dichloroethane (0.194)': 0.194}
+        translate = {'Dimethylsulfoxide (0.444)': 0.444, 'Water (1.0)': 1.0, 'Dichloromethane (0.309)': 0.309, 'Acetonitrile (0.46)': 0.46, 'Toluene (0.099)': 0.099, 'Ethanol (0.654)': 0.654, 'Chloroform (0.259)': 0.259, 'Methanol (0.762)': 0.762, 'Dimethylformamide (0.386)': 0.386, 'Cyclochexane (0.006)': 0.006, 'Hexane (0.009)': 0.009, 'Tetrahidrofurane (0.605)': 0.605, 'Methyl Cianide (0.46)': 0.46, 'Acetate (0.355)': 0.355, 'Methyl Phenil (0.099)': 0.099, 'Isopropanol (0.546)': 0.546, 'Dioxane (0.164)': 0.164, 'mXylene (0.074)': 0.074, 'Chlorobenzene (0.333)': 0.333, 'Ethyl Acetate (0.228)': 0.228, 'DiethylEter (0.117)': 0.117, 'Octanol (0.537)': 0.537, 'Nitrobenzila (0.333)': 0.333, 'Benzene (0.111)': 0.111, 'Dichloroethane (0.194)': 0.194}
 
         return translate[solvent]
     
@@ -95,4 +123,22 @@ class Backend:
         self.lgbm_maxem = pickle.load(open(path_lgbm_maxem, 'rb'))
         self.rf_maxem = pickle.load(open(path_rf_maxem, 'rb'))
         self.xgb_maxem = pickle.load(open(path_xgb_maxem, 'rb'))
+
+    def _loadShap(self):
+        path_shap_lgbm_maxabs = 'Models/shap/lgbm_explainer_abs.pkl'
+        path_shap_rf_maxabs = 'Models/shap/rf_explainer_abs.pkl'
+        path_shap_xgb_maxabs = 'Models/shap/xgb_explainer_abs.pkl'
+
+        path_shap_lgbm_maxem = 'Models/shap/lgbm_explainer_em.pkl'
+        path_shap_rf_maxem = 'Models/shap/rf_explainer_em.pkl'
+        path_shap_xgb_maxem = 'Models/shap/xgb_explainer_em.pkl'
+
+        self.shap_lgbm_maxabs = pickle.load(open(path_shap_lgbm_maxabs, 'rb'))
+        self.shap_rf_maxabs = pickle.load(open(path_shap_rf_maxabs, 'rb'))
+        self.shap_xgb_maxabs = pickle.load(open(path_shap_xgb_maxabs, 'rb'))
+
+        self.shap_lgbm_maxem = pickle.load(open(path_shap_lgbm_maxem, 'rb'))
+        self.shap_rf_maxem = pickle.load(open(path_shap_rf_maxem, 'rb'))
+        self.shap_xgb_maxem = pickle.load(open(path_shap_xgb_maxem, 'rb'))
+
 
--- srcCode/frontend.py
+++ srcCode/frontend.py
@@ -15,10 +15,16 @@
 ### IMPORTS ###
 
 import streamlit as st
+import matplotlib.pyplot as plt
+import shap
+import numpy as np
+from pandas import DataFrame
 from streamlit_option_menu import option_menu
 from streamlit_theme import st_theme
 from streamlit_ketcher import st_ketcher
 from warnings import filterwarnings
+from .drawSmiles import *
+import cairosvg
 filterwarnings('ignore')
 
 ### SELF IMPORTS ###
@@ -38,6 +44,9 @@ class Frontend(Backend):
 
         if 'clickedSmiles' not in st.session_state:
             st.session_state.clickedSmiles = False
+
+        if 'display_substructures' not in st.session_state:
+            st.session_state.display_substructures = False
 
         texts = Texts()
 
@@ -80,6 +89,7 @@ class Frontend(Backend):
             st.markdown('---')
             if st.button('Predict'):
                 Frontend.predict(self)
+                Frontend.shap_analysis(self)
 
         elif navbar == 'About':
             st.header('About ℹ️')
@@ -148,6 +158,9 @@ class Frontend(Backend):
     def click_button(self):
         st.session_state.clicked = True
 
+    def click_button_substructure(self):
+        st.session_state.display_substructures = True
+
     def inputSmiles(self):
         
         st.button("Draw molecule instead...", on_click=self.click_button)
@@ -201,40 +214,69 @@ class Frontend(Backend):
                         self.smiles = ''
 
     def selectSolvent(self):
-        solvents = ['Dimethylsulfoxide (0.444)', 'Water (1.0)', 'Dichloromethane (0.309)', 'Acetonitrile (0.46)', 'Tolune (0.099)', 'Ethanol (0.654)', 'Chloroform (0.259)', 'Methanol (0.762)', 'Dimethylformamide (0.386)', 'Cyclochexane (0.006)', 'Hexane (0.009)', 'Tetrahidrofurane (0.605)', 'Methyl Cianide (0.46)', 'Acetate (0.355)', 'Methyl Phenil (0.099)', 'Isopropanol (0.546)', 'Dioxane (0.164)', 'mXylene (0.074)', 'Chlorobenzene (0.333)', 'Ethyl Acetate (0.228)', 'DiethylEter (0.117)', 'Octanol (0.537)', 'Nitrobenzila (0.333)', 'Benzene (0.111)', 'Dichloroethane (0.194)']
+        solvents = ['Dimethylsulfoxide (0.444)', 'Water (1.0)', 'Dichloromethane (0.309)', 'Acetonitrile (0.46)', 'Toluene (0.099)', 'Ethanol (0.654)', 'Chloroform (0.259)', 'Methanol (0.762)', 'Dimethylformamide (0.386)', 'Cyclochexane (0.006)', 'Hexane (0.009)', 'Tetrahidrofurane (0.605)', 'Methyl Cianide (0.46)', 'Acetate (0.355)', 'Methyl Phenil (0.099)', 'Isopropanol (0.546)', 'Dioxane (0.164)', 'mXylene (0.074)', 'Chlorobenzene (0.333)', 'Ethyl Acetate (0.228)', 'DiethylEter (0.117)', 'Octanol (0.537)', 'Nitrobenzila (0.333)', 'Benzene (0.111)', 'Dichloroethane (0.194)']
 
         solvent = st.selectbox('Select Solvent', solvents)
 
         self.etn = Backend._solventToEtn(self, solvent)
         
-
     def predict(self):
         st.markdown('### Predicting... 🧠⚙️')
 
         fp, bit = Backend._generateMF(self, self.smiles)
         fp = fp.reshape(1, -1)
+
+
+        self.finger_print = fp
+        self.bit = bit
+
         input = Backend._PrepareInput(self, fp, self.etn)
 
         if self.mod == 'Random Forest':
             my_em = self.rf_maxem.predict(input)
             my_abs = self.rf_maxabs.predict(input)
 
+            my_shap_em = self.shap_rf_maxem.shap_values(input)
+            my_shap_abs = self.shap_rf_maxabs.shap_values(input)
+
+            my_explainer_em = self.shap_rf_maxem.expected_value
+            my_explainer_abs = self.shap_rf_maxabs.expected_value
+
         elif self.mod == 'XGBoost':
             my_em = self.xgb_maxem.predict(input)
             my_abs = self.xgb_maxabs.predict(input)
+
+            my_shap_em = self.shap_xgb_maxem.shap_values(input)
+            my_shap_abs = self.shap_xgb_maxabs.shap_values(input)
+
+            my_explainer_em = self.shap_xgb_maxem.expected_value
+            my_explainer_abs = self.shap_xgb_maxabs.expected_value
 
         elif self.mod == 'LightGBM':
             my_em = self.lgbm_maxem.predict(input)
             my_abs = self.lgbm_maxabs.predict(input)
 
+            my_shap_em = self.shap_lgbm_maxem.shap_values(input)
+            my_shap_abs = self.shap_lgbm_maxabs.shap_values(input)
+
+            my_explainer_em = self.shap_lgbm_maxem.expected_value
+            my_explainer_abs = self.shap_lgbm_maxabs.expected_value
+
+        self.my_shap_em = my_shap_em
+        self.my_shap_abs = my_shap_abs
+        self.my_explainer_em = my_explainer_em
+        self.my_explainer_abs = my_explainer_abs
+
+        self.mf_used_in_model = input
+
         col1,col2 = st.columns(2)
 
         with col1:
-            st.markdown('#### Predicted max abs: {:.2f}nm'.format(my_abs[0]))
+            st.markdown('#### Predicted max abs: {:.3f}nm'.format(my_abs[0]))
         with col2:
-            st.markdown('#### Predicted max em: {:.2f}nm'.format(my_em[0]))
+            st.markdown('#### Predicted max em: {:.3f}nm'.format(my_em[0]))
 
-        st.markdown('<div style="text-align: center;"><h4>Predicted stokes shift: {:.2f} nm</div><h4>'.format(my_em[0] - my_abs[0]), unsafe_allow_html=True)
+        st.markdown('<div style="text-align: center;"><h4>Predicted stokes shift: {:.3f} nm</div><h4>'.format(my_em[0] - my_abs[0]), unsafe_allow_html=True)
 
         if self.max_percentage >= 70:        
             st.markdown('<div style="text-align: center;"><h4>Similarity: <span style="color:green">{:.2f}%</span></h4>'.format(self.max_percentage), unsafe_allow_html=True)  
@@ -245,4 +287,154 @@ class Frontend(Backend):
         else:
             st.markdown('<div style="text-align: center;"><h4>Similarity: <span style="color:red">{:.2f}%</span></h4></div>'.format(self.max_percentage), unsafe_allow_html=True)
 
+        st.markdown('---')
+
+
+    def shap_analysis(self):
+        st.markdown('### SHAP Analysis 📊')
+
+        col3, col4 = st.columns(2)
+
+        st.markdown('#### Max Absorption')
+
+        features_names = [
+        f"{str(self.mf_used_in_model[0][i])[0]} {self.features_names[i]}" if self.features_names[i] != "etn" 
+        else self.features_names[i]
+        for i in range(len(self.features_names))
+        ]
+
+        shap_values_abs_plotter = shap.Explanation(self.my_shap_abs, self.my_explainer_abs, feature_names=features_names)
+        
+        fig,ax = plt.subplots()
+        shap.waterfall_plot(shap_values_abs_plotter[0], max_display=10, show=False)
+        st.pyplot(fig)
+
+        top_indices_abs = np.argsort(-np.abs(shap_values_abs_plotter.values[0]))[:10]
+        ten_features_abs = [self.features_names[i] for i in top_indices_abs]
+        bits = [x.split('_')[1] for x in ten_features_abs if '_' in x]
+
+        st.markdown('---')
+
+        images_presence = []
+        images_absence = []
+        
+        labels_presence = []
+        labels_absence = []
+
+        dl = 4
+        
+        for j in bits:
+            bit_number = int(j)
+            smiles = self.smiles
+            mol = Chem.MolFromSmiles(smiles)
+            addcords(mol)
+            bi = self.bit
+            fp = self.finger_print
+            try:
+                img = draw_substructure(mol, bi, bit_number, 400, detail_level=dl)
+                images_presence.append(img)
+                labels_presence.append('MF_{}'.format(bit_number))
+            except KeyError:
+                molNumber = self.df_drawn.loc[self.df_drawn['MF_{}'.format(bit_number)] == 1].index[0]
+                smiles = self.data['smiles'].iloc[molNumber]
+                mol = Chem.MolFromSmiles(smiles)
+                addcords(mol)
+                bi = {}
+                fp = AllChem.GetMorganFingerprintAsBitVect(mol, 4, bitInfo=bi, nBits=2048)
                 
+                img = draw_substructure(mol, bi, bit_number, 400, detail_level=dl)
+                images_absence.append(img)
+                labels_absence.append('MF_{}'.format(bit_number))
+        
+        st.markdown('#### Presence of MF')
+        
+        max_columns = 5
+        num_columns = min(len(images_presence), max_columns)
+
+        cols_presence = st.columns(num_columns)
+
+        for i, img in enumerate(images_presence):
+            with cols_presence[i % num_columns]:  
+                st.text(labels_presence[i])  
+                st.image(img, use_container_width=True)
+
+        st.markdown('---')
+
+        st.markdown('#### Absence of MF')
+
+        cols_absence = st.columns(num_columns)
+
+        for i, img in enumerate(images_absence):
+            with cols_absence[i % num_columns]:  
+                st.text(labels_absence[i])  
+                st.image(img, use_container_width=True)
+
+        st.markdown('---')
+
+        st.markdown('#### Max Emission')
+        
+        shap_values_em_plotter = shap.Explanation(self.my_shap_em, self.my_explainer_em, feature_names=features_names)
+        fig,ax = plt.subplots()
+        shap.waterfall_plot(shap_values_em_plotter[0], max_display=10, show=False)
+        st.pyplot(fig)
+
+        top_indices_abs = np.argsort(-np.abs(shap_values_em_plotter.values[0]))[:10]
+        ten_features_abs = [shap_values_em_plotter.feature_names[i] for i in top_indices_abs]
+        bits = [x.split('_')[1] for x in ten_features_abs if '_' in x]
+
+        images_presence = []
+        images_absence = []
+        
+        labels_presence = []
+        labels_absence = []
+
+        for j in bits:
+            bit_number = int(j)
+            smiles = self.smiles
+            mol = Chem.MolFromSmiles(smiles)
+            addcords(mol)
+            bi = self.bit
+            fp = self.finger_print
+            
+            
+            try:
+                img = draw_substructure(mol, bi, bit_number, 400, detail_level=dl)
+                images_presence.append(img)
+                labels_presence.append('MF_{}'.format(bit_number))
+            except KeyError:
+                molNumber = self.df_drawn.loc[self.df_drawn['MF_{}'.format(bit_number)] == 1].index[0]
+                smiles = self.data['smiles'].iloc[molNumber]
+                mol = Chem.MolFromSmiles(smiles)
+                addcords(mol)
+                bi = {}
+                fp = AllChem.GetMorganFingerprintAsBitVect(mol, 4, bitInfo=bi, nBits=2048)
+                
+                img = draw_substructure(mol, bi, bit_number, 400, detail_level=dl)
+                images_absence.append(img)
+                labels_absence.append('MF_{}'.format(bit_number))
+            
+        st.markdown('#### Presence of MF')
+        
+        max_columns = 5
+        num_columns = min(len(images_presence), max_columns)
+
+        cols_presence = st.columns(num_columns)
+
+        for i, img in enumerate(images_presence):
+            with cols_presence[i % num_columns]:  
+                st.text(labels_presence[i])  
+                st.image(img, use_container_width=True)
+
+        st.markdown('---')
+
+        st.markdown('#### Absence of MF')
+
+        cols_absence = st.columns(num_columns)
+
+        for i, img in enumerate(images_absence):
+            with cols_absence[i % num_columns]:  
+                st.text(labels_absence[i])  
+                st.image(img, use_container_width=True)
+
+        st.markdown('---')
+
